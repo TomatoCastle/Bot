b5d6a7fb210038217a5d8ea3ca5ce29a947f4315
tomato <nomu.tomato.zyusu@gmail.com>
2018-03-06 16:11:23 +0900

    とりあえず形だけ作成

--- wether/getWether.py
+++ wether/getWether.py
@@ -1,2 +1,21 @@
-class getWether:
-    pass
+import threading
+import time
+
+class getWether(threading.Thread):
+    def __init__(self,uri):
+        self.check_type_uri(uri)
+        self.uri = uri
+        #self.wether = OnePleaceWether.OnePleaceWether(uri)
+
+    def check_type_uri(self,uri):
+        pass
+
+    def run(self):
+        pass
+
+    def get_wether(self):
+        pass
+    
+    def check_uri_type(self, uri):
+        if not isinstance(uri,str):
+            raise TypeError('\"uri\"\'s type must be \"str\"')
