b71bd156d5cc22f4d96bf51d8fce27754810054f
tomato <nomu.tomato.zyusu@gmail.com>
2018-03-07 18:58:48 +0900

    [add]メソッドのひな型を追加

--- wether/getWether.py
+++ wether/getWether.py
@@ -7,14 +7,24 @@ class getWether(threading.Thread):
         self.check_type_uri(uri)
         self.uri = uri
         self.wether = op.OnePleaceWether(uri)
+        self.now_wehter_json = None
 
     def run(self):
-        wether = self.wether.get_wether_with_update()
+        pass
         
+    def check_wether_is_rain(self):
+        pass
+
+    def check_get_time(self):
+        return None
 
     def get_wether(self):
+        #wether = self.wether.get_wether_with_update()
         pass
     
+    def update_uri(self):
+        pass
+
     def check_type_uri(self, uri):
         if not isinstance(uri,str):
             raise TypeError('\"uri\"\'s type must be \"str\"')
