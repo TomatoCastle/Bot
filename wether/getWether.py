31499346e8af2fc89c91f8ecebf2a435a2dad1fc
tomato <nomu.tomato.zyusu@gmail.com>
2018-03-09 11:56:36 +0900

    時間になっているかどうかを調べるメソッド型追加

--- wether/getWether.py
+++ wether/getWether.py
@@ -1,21 +1,24 @@
 import threading
 import time
 import OnePleaceWether as op
+import datetime
 
 class getWether(threading.Thread):
-    def __init__(self,uri):
+    def __init__(self,uri,pushing_time):
         self.check_type_uri(uri)
         self.uri = uri
+        self.check_type_time(pushing_time)
+        self.pushing_time = pushing_time
         self.wether = op.OnePleaceWether(uri)
         self.now_wehter_json = None
 
     def run(self):
         pass
-        
+
     def is_rain(self):
         pass
 
-    def check_get_time(self):
+    def check_get_time(self,pushing_time):
         return None
 
     def get_wether(self):
@@ -24,6 +27,10 @@ class getWether(threading.Thread):
     
     def update_uri(self):
         pass
+
+    def check_type_time(self, pushing_time):
+        if not isinstance(uri,datetime.datetime):
+            raise TypeError('\"Callingtime\"\'s type must be \"datetime\"')
 
     def check_type_uri(self, uri):
         if not isinstance(uri,str):
